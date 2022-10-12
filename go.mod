3edbba4bc05ca128d07ac9dd2d4e17adca998544
GalvinGao <me@galvingao.com>
2022-10-11 20:31:58 -0400

    feat: add template

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require github.com/gorilla/mux v1.8.0
 
 require (
 	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/go-ozzo/ozzo-validation v3.6.0+incompatible // indirect
 	github.com/gofiber/fiber/v2 v2.38.1 // indirect
 	github.com/klauspost/compress v1.15.0 // indirect
 	github.com/labstack/echo/v4 v4.9.0 // indirect
--- main.go
+++ main.go
@@ -1,15 +1,17 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
+	"regexp"
 
+	validation "github.com/go-ozzo/ozzo-validation"
 	"github.com/gofiber/fiber/v2"
 )
 
 const (
 	HtmlContent = `<html>
   <head>
-    <meta name="go-import" content="github.com/%s/%s git https://github.com/%s/%s.git">
+    <meta name="go-import" content="github.com/penguin-statistics/{{ .pkg }} git https://github.com/penguin-statistics/{{ .pkg }}.git">
   </head>
 </html>`
 )
@@ -17,15 +19,26 @@ const (
 func main() {
 	app := fiber.New()
 
+	pkgMatcher := regexp.MustCompile(`^[a-z0-9-]+$`)
+
+	tmpl := template.Must(template.New("index").Parse(HtmlContent))
+
 	app.Get("/:pkg", func(c *fiber.Ctx) error {
-		project := "penguin-statistics"
 		pkg := c.Params("pkg")
 
-		if project == "" || pkg == "" {
+		err := validation.Validate(pkg,
+			validation.Required,
+			validation.Length(1, 64),
+			validation.NotIn("favicon.ico", "robots.txt"),
+			validation.Match(pkgMatcher),
+		)
+		if err != nil {
 			return c.SendStatus(fiber.StatusBadRequest)
 		}
 
-		return c.SendString(fmt.Sprintf(HtmlContent, project, pkg, project, pkg))
+		return tmpl.Execute(c, fiber.Map{
+			"pkg": pkg,
+		})
 	})
 
 	if err := app.Listen(":3000"); err != nil {
